5efdb8e93b9a5068382aff62e6a6dff976e298f3
drec-stack <ruzalmuhamedgaleev@gmail.com>
2025-10-30 22:54:36 +0300

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
+  reactStrictMode: true,
   images: {
-    unoptimized: true
+    unoptimized: true,
+    domains: ['cdn.dummyjson.com', 'i.dummyjson.com'],
   },
+  trailingSlash: true,
   basePath: process.env.NODE_ENV === 'production' ? '/product-catalog' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/product-catalog/' : '',
 }
 
 module.exports = nextConfig
